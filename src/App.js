ef7dcf972c19efde13423efa93cd70cf0fdae9d7
sahinyasasi <sahinya.slc@gmail.com>
2021-01-30 11:18:05 +0530

    intro(themes usage)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
-import { Button } from "@material-ui/core";
-
 import "./App.css";
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  helloThereStyle: {
+    fontStyle: "oblique",
+    color: "red",
+    fontSize: "50px",
+  },
+  buttonStyles: {
+    color: "green",
+    border: 0,
+  },
+});
 
 export default function () {
+  const classes = useStyles();
   return (
     <div className='App'>
-      <Button></Button>
+      <Typography className={classes.helloThereStyle} variant='h1'>
+        Hello Der!
+      </Typography>
+      <Button
+        className={classes.buttonStyles}
+        color='secondary'
+        variant='outlined'
+      >
+        This is our button
+      </Button>
     </div>
   );
 }
